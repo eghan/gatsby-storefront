52484d5bcffe8c252885fdadc98f3ffe425a139e
eghan <eghant@gmail.com>
2018-11-24 20:01:18 -0800

    add contact form stub

---
+++ src/pages/contact.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <h1>Hi stuff about Artofactory</h1>
+
+    <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field">
+        <input name="name" placeholder="Your name"  type="text"/>
+        <button>Send</button>
+    </form>
+    <p>Cool stuff yall</p>Lorem ipsum dolor amet echo park man braid pug venmo,
+    raw denim literally trust fund drinking vinegar pour-over letterpress tilde
+    kale chips disrupt wolf sustainable. Helvetica typewriter cornhole vaporware
+    copper mug yr. Single-origin coffee tousled banjo, meditation scenester
+    leggings microdosing chillwave venmo cloud bread kogi salvia trust fund
+    cronut. Chillwave hella godard flannel meh. Brooklyn tousled drinking
+    vinegar seitan ramps. Yr kinfolk fixie chambray typewriter gochujang. Squid
+    tofu pour-over, lumbersexual taiyaki try-hard lomo intelligentsia literally
+    quinoa. Meditation neutra taiyaki freegan literally. 90's truffaut poutine,
+    pork belly taiyaki church-key tacos skateboard tumeric. Vexillologist swag
+    tbh cardigan lyft shoreditch waistcoat pork belly taxidermy humblebrag tote
+    bag pickled copper mug artisan small batch. Venmo 90's craft beer ugh,
+    selvage cold-pressed keytar celiac freegan keffiyeh pork belly. Listicle
+    cronut yr tilde, fingerstache jianbing pitchfork tote bag church-key lyft
+    edison bulb iceland 90's asymmetrical neutra. Brooklyn taiyaki authentic
+    chia asymmetrical. Hoodie YOLO everyday carry, gochujang twee taiyaki
+    waistcoat artisan XOXO deep v live-edge.
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default About

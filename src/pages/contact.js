b4fa5b1853fc756dfb1f96e5336cc32d99886c72
eghan <eghant@gmail.com>
2018-11-25 16:49:03 -0800

    add styling to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,82 +1,114 @@
 import React from 'react'
 import { navigateTo } from 'gatsby-link'
 import Layout from '../components/layout'
+import styled from 'styled-components'
 
+const Container = styled.div`
+    text-align: left;
+    margin: 2rem auto;
+    max-width: 90%;
+    border: 1px dashed silver;
+`
+const TextArea = styled.textarea`
+    height: 220px;
+    width: 50%;
+`
 
 function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+    return Object.keys(data)
+        .map(
+            key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        )
+        .join('&')
 }
 
 export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
-    })
-      .then(() => navigateTo(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+    handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...this.state,
+            }),
+        })
+            .then(() => navigateTo(form.getAttribute('action')))
+            .catch(error => alert(error))
+    }
 
-  render() {
-    return (
-      <Layout>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Container>
+                    <h1>Contact</h1>
+                    <form
+                        name="contact"
+                        method="post"
+                        action="/thanks/"
+                        data-netlify="true"
+                        data-netlify-honeypot="bot-field"
+                        onSubmit={this.handleSubmit}
+                    >
+                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                        <input type="hidden" name="form-name" value="contact" />
+                        <p hidden>
+                            <label>
+                                Don’t fill this out:{' '}
+                                <input
+                                    name="bot-field"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Your name:
+                                <br />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Your email:
+                                <br />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Message:
+                                <br />
+                                <TextArea
+                                    name="message"
+                                    onChange={this.handleChange}
+                                />
+                            </label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
+                    </form>
+                </Container>
+            </Layout>
+        )
+    }
 }

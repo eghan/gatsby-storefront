fc509f64f65eb2a2a5c07e394055ee5ee02d9b47
eghan <eghant@gmail.com>
2018-12-06 23:36:08 -0800

    add regass/grid

--- src/templates/etsy-page-template.js
+++ src/templates/etsy-page-template.js
@@ -4,9 +4,10 @@ import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import styled from 'styled-components'
 
+import { Flex, Box } from '@rebass/grid'
 import PaypalExpressBtn from 'react-paypal-express-checkout'
 
-const Container = styled.div`
+const Container = styled(Flex)`
   margin: 0rem auto;
   max-width: 90%;
   border: 1px dashed silver;
@@ -19,16 +20,18 @@ const Info = styled.div`
   border: 1px dashed silver;
 `
 const Tag = styled.button`
-  padding: 0rem 0.2rem;
+  padding: .01rem .6rem;
   display: inline-block;
   border: 0.5px dashed silver;
   font-size: 0.6em;
   text-decoration: none;
-  margin-bottom: 0.1em;
+  margin-bottom: 0.01em;
   &:focus {
     outline: 0;
   }
   &:hover {
+
+  border: 0.5px solid black;
     background-color: #f5f5f5;
   }
   -webkit-transition-duration: 0.8s; /* Safari */
@@ -40,13 +43,16 @@ const Price = styled.div`
   display: inline;
 `
 const Tagbox = styled.div`
-  float: right;
-  max-width: 50%;
+  width: 60%;
+  text-align: center;
+  float: left;
+
+  border: 0.5px dashed silver;
 `
 const Photo = styled(Img)`
-  padding: 2px 2px;
-  float: left;
+  padding: 2px 2px 5px 5px;
   display: inline-block;
+  float: right;
 `
 const Payment = styled.div`
   padding: 2px 2px;
@@ -87,27 +93,30 @@ export default ({ data }) => {
   return (
     <Layout>
       <Container>
-        <Photo
-          title={`Photo by Eghan Thompson`}
-          fixed={image.childImageSharp.fixed}
-        />
-        <Tagbox>{tagList}</Tagbox>
-        <Info>
-          <p>{name}</p>
-          {description}
-        </Info>
-
-        {' '}
-        
-        <Payment>
-          <Price>{price} $</Price>
-          <PaypalExpressBtn
-            client={client}
-            currency={'USD'}
-            total={price}
-            style={style}
-          />
-        </Payment>
+          <Box width={1 / 2} px={2} pr={18} p={5} alignSelf='flex-end'>
+            <Photo
+              title={`Photo by Eghan Thompson`}
+              fixed={image.childImageSharp.fixed}
+            />
+          </Box>
+          <Box width={1 / 2} px={2} p={5}>
+            <Info>
+              <p>{name}</p>
+              {description}
+            </Info>
+            {' '}
+            <Tagbox>{tagList}</Tagbox>
+            <Payment>
+              <Price>{price} $</Price>
+              <h5>free shipping</h5>
+              <PaypalExpressBtn
+                client={client}
+                currency={'USD'}
+                total={price}
+                style={style}
+              />
+            </Payment>
+          </Box>
       </Container>
     </Layout>
   )
@@ -120,7 +129,7 @@ export const query = graphql`
       price: PRICE
       image {
         childImageSharp {
-          fixed(width: 300) {
+          fixed(width: 500) {
             ...GatsbyImageSharpFixed
           }
         }

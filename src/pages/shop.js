72b63103c157a402fbe1dca745e0481846f3dba3
eghan <eghant@gmail.com>
2018-12-13 01:07:27 -0800

    styling for grid, including mobile

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -4,38 +4,41 @@ import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import styled from 'styled-components'
 
-const Container = styled.div`
+import { Flex, Box } from '@rebass/grid'
+
+const Container = styled(Flex)`
   display: inline-block;
   text-align: center;
   width: 100%;
   margin: 0.5rem auto;
 `
+const Item = styled(Box)`
+  display: inline-block;
+`
 const Details = styled.div`
+  margin: 0.3em 0.3em;
   display: block;
-  color: black;
-  font-size: 0.8em;
-  text-align: left;
-  text-decoration: none;
-  &:focus {
-    outline: 0;
-  }
-  &:hover {
-    background-color: #f5f5f5;
-  }
-  -webkit-transition-duration: 0.8s; /* Safari */
-  transition-duration: 0.8s;
+  font-size: 1em;
+  text-align: left;  
+  @media (max-width: 750px) {
+      display: none
+    }
 `
 const Price = styled.div`
   float: right;
 `
 const Photo = styled(Img)`
-  padding: 1em 1em;
-  max-width: 200px;
-  max-height: 200px;
-  display: inline-block;
+  width: 300px;
+  height: 300px;
+  padding: 0.5em 0.5em;
+  @media (max-width: 1040px) {
+      padding: 0em 0em;
+  width: 125px;
+  height: 125px;
+    }
 `
 const PhotoLink = styled(Link)`
-  padding: 1em 1em;
+  margin: 0.5em 0.5em;
   display: inline-block;
   font-size: 0.8em;
   text-decoration: none;
@@ -49,7 +52,11 @@ function renderTagMatches(data) {
       //if (item.node.data.name === 'photoset') {return}
       matchList = [
         ...matchList,
-        [item.node.data.name, item.node.data.image.localFiles[0], item.node.data.price],
+        [
+          item.node.data.name,
+          item.node.data.image.localFiles[0],
+          item.node.data.price,
+        ],
       ]
       return
     })
@@ -57,21 +64,28 @@ function renderTagMatches(data) {
 
   if (data.etsy !== null) {
     data.etsy.edges.forEach(item => {
-      matchList = [...matchList, [item.node.name, item.node.image, item.node.price]]
+      matchList = [
+        ...matchList,
+        [item.node.name, item.node.image, item.node.price],
+      ]
       return
     })
   }
 
   let output = matchList.map((match, index) => (
     <PhotoLink to={match[0]} key={match[0]}>
-      {/* <Tag>{match[0]}</Tag> */}
       <Photo
+        style={{ backgroundSize: 'cover' }}
         key={index}
         title={match[1].childImageSharp.id}
-        fixed={match[1].childImageSharp.fixed}
+        fluid={match[1].childImageSharp.fluid}
       />
-      <Details>{ (match[0].length > 24) ? match[0].substring(0,24).concat('...') : match[0]} 
-      <Price>{ match[2] > 0 && '$' + match[2] }</Price></Details>
+      <Details>
+        {match[0].length > 24
+          ? match[0].substring(0, 24).concat('...')
+          : match[0]}
+        <Price>{match[2] > 0 && '$' + match[2]}</Price>
+      </Details>
     </PhotoLink>
   ))
 
@@ -98,8 +112,8 @@ export const query = graphql`
               localFiles {
                 childImageSharp {
                   id
-                  fixed(width: 300) {
-                    ...GatsbyImageSharpFixed
+                  fluid(maxWidth: 400) {
+                    ...GatsbyImageSharpFluid
                   }
                 }
               }
@@ -119,8 +133,8 @@ export const query = graphql`
           image {
             childImageSharp {
               id
-              fixed(width: 300) {
-                ...GatsbyImageSharpFixed
+              fluid(maxWidth: 400) {
+                ...GatsbyImageSharpFluid
               }
             }
           }

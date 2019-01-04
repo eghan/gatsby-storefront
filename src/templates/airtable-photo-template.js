6c16eb93bc0d5e033c0457374d0ef0368ce226e0
eghan <eghant@gmail.com>
2019-01-04 12:29:36 -0800

    mobile styling, image sizing and grid breaks

--- src/templates/airtable-photo-template.js
+++ src/templates/airtable-photo-template.js
@@ -5,32 +5,38 @@ import Layout from '../components/layout'
 import styled from 'styled-components'
 
 const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   margin: 0rem auto;
-  max-width: 90%;
-  border: 1px dashed silver;
-  align-items: center;
+  padding: 1em 1em;  
+  @media (max-width: 750px) {
+    grid-template-columns: 1fr;
+    padding: 0em 0em;
+  }
+  /*max-width: 1fr;*/
+  /*border: 1px dashed silver;*/
+  /*align-items: center;*/
 `
 const Photos = styled.div`
-  margin: 0rem auto;
-  max-width: 400px;
-  display: inline-block;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  float: center;
-  border: 1px dashed silver;
+  /*margin: 0rem auto;  */
+  grid-column: 1;
+  border: 3px dashed silver;
 `
 const Info = styled.div`
-  padding: 0.05rem 0.5rem;
-  margin: 1rem auto;
-  display: inline-block;
-  flex-direction: column;
-  border: 1px dashed silver;
+  width: 50vw;
+  padding: 1em 1em;
+  border: 1px dashed silver; 
+  @media (max-width: 750px) {
+    width: 100vw;
+  }
 `
 const Photo = styled(Img)`
-  padding: 2px 2px;
-  float: center;
-  display: inline-block;
+  float: right;
+  width: 50vw; 
+  @media (max-width: 750px) {
+    width: 100vw;
+  }
+  /*padding: 0.5rem 0.7rem;*/
 `
 
 export default ({ data }) => {
@@ -40,16 +46,14 @@ export default ({ data }) => {
     <Layout>
       <Container>
         <Photos>
-          <div className={node.id}>
-            <Photo
-              title={`Photo by Eghan Thompson`}
-              fixed={node.childImageSharp.fixed}
-            />
-          </div>
+          <Photo
+            title={`Photo by Eghan Thompson`}
+            fluid={node.childImageSharp.fluid}
+          />
         </Photos>
         <Info>
-          This unique creation has been sold into the world. To inquire about
-          comissioning a piece like this one, click the link below.
+          Feel free to to inquire about
+          comissioning a piece like this one, with this handy form.
           <br />
           <br />
           <p>{node.name}</p>
@@ -72,8 +76,8 @@ export const query = graphql`
           id
           name
           childImageSharp {
-            fixed {
-              ...GatsbyImageSharpFixed
+            fluid(quality: 100, maxWidth: 900) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
             }
           }
         }

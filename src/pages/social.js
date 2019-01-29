179aecdf29b2c95e829402ddf0f788539d4bfbff
eghan <eghant@gmail.com>
2019-01-28 17:26:44 -0800

    page for social

---
+++ src/pages/social.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+
+const SocialDiv = styled.div`
+    padding: 30px;
+`
+
+const About = () => (
+  <Layout>
+    <SocialDiv>
+    Links to social spaces
+    </SocialDiv>
+  </Layout>
+)
+
+export default About

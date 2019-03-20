d08c69d23386082f443130e8e74775d3b69306f9
eghan <eghant@gmail.com>
2019-03-20 16:37:56 -0700

    cleanup

--- src/templates/airtable-page-template.js
+++ src/templates/airtable-page-template.js
@@ -6,65 +6,9 @@ import styled from 'styled-components'
 import Layout from '../components/layout'
 import Product from '../components/product'
 
-const Container = styled.div`
-  margin: 0rem auto;
-  max-width: 90%;
-  border: 1px dashed silver;
-`
-const Photos = styled.div`
-  margin: 0rem auto;
-  max-width: 400px;
-  display: inline-block;
-  flex-direction: column;
-  align-items: center;
-  justify-content: left;
-  float: left;
-  border: 1px dashed silver;
-`
-const Info = styled.div`
-  padding: 0.05rem 0.5rem;
-  margin: 1rem auto;
-  display: inline-block;
-  flex-direction: column;
-  border: 1px dashed silver;
-`
-const Tag = styled.button`
-  padding: 0rem 0.2rem;
-  display: inline-block;
-  border: 0.5px dashed silver;
-  font-size: 0.6em;
-  text-decoration: none;
-  margin-bottom: 0.1em;
-  &:focus {
-    outline: 0;
-  }
-  &:hover {
-    background-color: #f5f5f5;
-  }
-  -webkit-transition-duration: 0.8s; /* Safari */
-  transition-duration: 0.8s;
-`
-const Price = styled.div`
-  float: right;
-  font-size: 0.9em;
-`
-const Tagbox = styled.div`
-  float: right;
-  max-width: 50%;
-`
-const Photo = styled(Img)`
-  padding: 2px 2px;
-  float: left;
-  display: inline-block;
-`
-
 export default ({ data }) => {
   // eslint-disable-next-line
   const { name, discription, price, tags = [], photo} = data.airtable.data
-  // gives tags array default value in case graphql returns non value
-  // TODO: this may be refactorable using destructuring and default values
-  // gatsby(JSX) build fails(passes develop) when tags is not present
-  // see issue #3344 https://github.com/gatsbyjs/gatsby/issues/3344
 
   const product = {
     name,
@@ -74,9 +18,7 @@ export default ({ data }) => {
     image: photo.localFiles[0].childImageSharp
   }
 
-  return (
-      <Product item={product} />
-  )
+  return ( <Product item={product} /> )
 }
 export const query = graphql`
   query airtableData($name: String!) {

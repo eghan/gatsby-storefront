303ba425a229b644b40f751ab3e5caaf0eb31dd1
eghan <eghant@gmail.com>
2019-08-12 15:14:00 -0700

    removed layout component for global state context

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import Layout from '../components/layout'
 import styled from 'styled-components'
 import { Location } from '@reach/router'
 
@@ -101,7 +100,7 @@ function renderTagMatches(data) {
 
 export default ({ data }) => {
   return (
-    <Layout>
+    <>
       <Tagbar />
       <Container>
       <Location>
@@ -113,7 +112,7 @@ export default ({ data }) => {
     </Location>
         {renderTagMatches(data)}
       </Container>
-    </Layout>
+    </>
   )
 }
 export const query = graphql`

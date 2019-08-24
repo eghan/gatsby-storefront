0fba6b53d1d97df3699b2b8e46f70a31f344c203
eghan <eghant@gmail.com>
2019-08-24 14:29:49 -0700

    mobile styling

--- src/pages/social.js
+++ src/pages/social.js
@@ -11,24 +11,35 @@ const SocialDiv = styled.div`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 1vw;
   width: 100%;
+  @media (max-width: 750px) {
+    grid-template-columns: 1fr 1fr;
+    position: absolute;
+    left: 5vw;
+    margin: 0;
+  }
+`
+const Section = styled.div`
+  @media (max-width: 750px) {
+    display: none;
+  }
 `
 
 const SocialData = styled.div`
   display: grid;
   @media (max-width: 750px) {
-      grid-column: span 2;
-      height: 50vw;
+    grid-column: span 2;
+    height: 50vw;
   }
 `
 const Photo = styled(Img)`
   width: 300px;
   height: 300px;
 
-// method to access picture properties directly
-    picture {
-     overflow: hidden;
-     width: 100px;
-    }
+  // method to access picture properties directly
+  picture {
+    overflow: hidden;
+    width: 100px;
+  }
 
   @media (max-width: 1040px) {
     padding: 0em 0em;
@@ -39,24 +50,30 @@ const Photo = styled(Img)`
 
 const About = () => {
   const data = useStaticQuery(graphql`
-  query SocialQuery {
-    allAirtable {
-      edges {
-        node {
-          data {
-            name
-            section
-            details
-            photo {
-              localFiles {
-                name
-                id
-                childImageSharp {
-                  low: fluid(quality: 60, maxWidth: 400, maxHeight: 400, cropFocus: CENTER) {
-                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                  }
-                  high: fluid(quality: 100) {
-                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+    query SocialQuery {
+      allAirtable {
+        edges {
+          node {
+            data {
+              name
+              section
+              details
+              photo {
+                localFiles {
+                  name
+                  id
+                  childImageSharp {
+                    low: fluid(
+                      quality: 60
+                      maxWidth: 400
+                      maxHeight: 400
+                      cropFocus: CENTER
+                    ) {
+                      ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                    }
+                    high: fluid(quality: 100) {
+                      ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                    }
                   }
                 }
               }
@@ -65,29 +82,36 @@ const About = () => {
         }
       }
     }
-  }
-`)
+  `)
 
-  const Socials = data.allAirtable.edges.filter( edge => edge.node.data.name === "social" )
-  return(
+  const Socials = data.allAirtable.edges.filter(
+    edge => edge.node.data.name === 'social'
+  )
+  return (
     <SocialDiv>
-    {Socials.map( element => {
-      return (
-        <div>
-          <SocialData key={element.section}>
-          <a href={element.node.data.details} target="_blank" rel="noopener noreferrer">   
-            <Photo
+      {Socials.map(element => {
+        return (
+          <div>
+            <SocialData key={element.section}>
+              <a
+                href={element.node.data.details}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Photo
                   key={element.node.data.section}
                   title={element.node.data.section}
-                  fluid={element.node.data.photo.localFiles[0].childImageSharp.high}
-          />
-        </a>
-          </SocialData>{element.node.data.section}                
-
-        </div>
+                  fluid={
+                    element.node.data.photo.localFiles[0].childImageSharp.high
+                  }
+                />
+              </a>
+            </SocialData>
+            <Section>{element.node.data.section}</Section>
+          </div>
         )
       })}
-  </SocialDiv>
+    </SocialDiv>
   )
 }
 

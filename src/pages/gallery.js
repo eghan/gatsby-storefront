a3c47e319964b831d3c047387dadc674c49f9c16
eghan <eghant@gmail.com>
2019-09-03 16:58:24 -0700

    add carousel to gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
-
+import EmblaCarouselReact from 'embla-carousel-react'
 import Modal from '../components/modal'
 
 const location =
   typeof window !== `undefined` ? window.location.pathname : '/gallery'
+
+// import React, { useState, useCallback, useEffect } from 'react'
+
+
 
 const Box = styled.div`
   /*min-width: 350px;*/
@@ -28,31 +32,110 @@ const Box = styled.div`
 const Photo = styled(Img)`
   /*padding: 1em 1em;*/
   border: 1px solid black;
+  width: 300px;
+  height: 300px;
 `
 
+
+
+
+const Carousel = styled.div`
+  grid-gap: 1vw;
+  padding: 1vw;
+  display: grid;
+  grid-template-columns: 1fr 7fr 1fr;
+  margin: auto;
+`
+const CarouselNavigation = styled.button`
+  width: 10vw;
+`
+const CarouselCenter = styled.div`
+  grid-area: 1/2;
+`
+const CarouselPhoto = styled(Photo)`
+  height: 500px;
+  width: 500px;
+`
+
+const EmblaCarouselComponent = photos => {
+  const [embla, setEmbla] = useState(null)
+  const scrollPrev = useCallback(() => embla.scrollPrev(), [embla])
+  const scrollNext = useCallback(() => embla.scrollNext(), [embla])
+
+  useEffect(() => {
+    if (embla) {
+      embla.on('select', () => {
+        console.log(`Current index is ${embla.selectedScrollSnap()}`)
+      })
+    }
+  }, [embla])
+
+  return (
+    <Carousel>
+      {/* {JSON.stringify(photos.photos[0].node.data.photo.localFiles[0])} */}
+      <CarouselNavigation onClick={scrollPrev}>Previous</CarouselNavigation>
+      <EmblaCarouselReact
+        htmlTagName="div"
+        emblaRef={setEmbla}
+        options={{ loop: true, startIndex: 2 }}
+      >
+        <CarouselCenter style={{ display: 'flex' }}>
+          {photos.photos[0].node.data.photo.localFiles.map(img => (
+            <Modal
+              object={img}
+              doubleclick="true"
+              source={img.childImageSharp.low}
+              location={location}
+              name={img.name}
+              text="Inqure here"
+            >
+              <CarouselPhoto
+                fadeIn={true}
+                key={img.id}
+                title={`Photo by Eghan Thompson`}
+                fluid={img.childImageSharp.low}
+                style={{ flex: '0 0 100%' }}
+              />
+            </Modal>
+          ))}
+        </CarouselCenter>
+      </EmblaCarouselReact>
+
+      <CarouselNavigation onClick={scrollNext}>Next</CarouselNavigation>
+    </Carousel>
+  )
+}
+
 const IndexPage = ({ data }) => (
+  <>
+    <EmblaCarouselComponent
+      photos={data.allAirtable.edges.filter(
+        edge => edge.node.data.name === 'photoset'
+      )}
+    />
     <Box>
       {data.allAirtable.edges
         .filter(edge => edge.node.data.name === 'photoset')
         .map((edge, i) =>
           edge.node.data.photo.localFiles.map(img => (
-              <Modal
-                object={img}
-                source={img.childImageSharp.low}
-                location={location}
-                name={img.name}
-                text='Inqure here'
-              >
-                <Photo
-                  fadeIn={true}
-                  key={img.id}
-                  title={`Photo by Eghan Thompson`}
-                  fluid={img.childImageSharp.low}
-                />
-              </Modal>
+            <Modal
+              object={img}
+              source={img.childImageSharp.low}
+              location={location}
+              name={img.name}
+              text="Inqure here"
+            >
+              <Photo
+                fadeIn={true}
+                key={img.id}
+                title={`Photo by Eghan Thompson`}
+                fluid={img.childImageSharp.low}
+              />
+            </Modal>
           ))
         )}
     </Box>
+  </>
 )
 
 export default IndexPage

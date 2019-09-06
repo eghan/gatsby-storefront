b2af1d361c7cae9b34b47baf4e080d2c1e7cf319
eghan <eghant@gmail.com>
2019-09-05 20:47:04 -0700

    file linking for inquiries

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -138,7 +138,7 @@ export default class Contact extends React.Component {
               <input
                 type="text"
                 name="image_input"
-                value={imageTargetURL}
+                value="${imageTargetURL}"
                 onChange={this.handleChange} 
                 hidden
               />

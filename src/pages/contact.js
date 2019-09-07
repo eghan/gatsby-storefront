ff573774610569fc6bb2d069f1151e13ae4e4588
eghan <eghant@gmail.com>
2019-09-07 12:51:34 -0700

    styling and file linking for inquiries from contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,26 +4,48 @@ import styled from 'styled-components'
 import { Consumer } from '../components/context'
 import Img from 'gatsby-image'
 
+import { Button } from '../utils/global'
+
 const Container = styled.div`
+border: 1px solid dimgray;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   text-align: center;
   margin: 1em auto 5em auto;
-  max-width: 90%;
+  max-width: 80vw;
 `
+const GridLeft = styled.div`
+  display: grid;
+  padding: .7em;
+  grid-area: 1/1;
+`
+const GridRight = styled.div`
+  display: grid;
+  text-align: left;
+  padding: 1em;
+  grid-area: 1/2;
+`
+const SubmitButton = styled(Button)`
+  font-size: 1em;
+`
+
 const Title = styled.div`
-  padding: 0 1em 0 1em;
+  margin: auto;
+  text-align: center;
+  padding: 1.5em 0 0 0;
 `
 const ImageAlert = styled.div`
-  font-size: 0.8em;
+  margin: 3em;
+  text-align: center;
   padding: 1em;
 `
 const TextArea = styled.textarea`
   height: 200px;
-  width: 80%;
+  width: 100%;
 `
 const Photo = styled(Img)`
-  width: 200px;
+  width: 30vw;
   margin: auto;
-  padding: 5.2em;
   border: 1px solid black;
 `
 
@@ -36,19 +58,21 @@ const Inquery = () => (
         if (data.itemInquery !== false) {
           imageTargetURL = data.itemInquery[0].childImageSharp.low.src
           return (
-              <Photo
-                fadeIn={true}
-                key={data.itemInquery[0].id}
-                title={`Photo by Eghan Thompson`}
-                fluid={data.itemInquery[0].childImageSharp.low}
-              />
+            <Photo
+              fadeIn={true}
+              key={data.itemInquery[0].id}
+              title={`Photo by Eghan Thompson`}
+              fluid={data.itemInquery[0].childImageSharp.low}
+            />
           )
         }
         return (
           <div>
             <ImageAlert>
-              You can also contact me about a specific photo by using the
-              inquery button under the image
+              You can contact me about a specific photo by using the
+              <Button>inquiry</Button> button under the image(click to zoom), in
+              the <Button onClick={() => navigate('gallery')}>Gallery</Button>{' '}
+              or <Button>Home</Button> pages
             </ImageAlert>
           </div>
         )
@@ -61,7 +85,6 @@ function encode(data) {
   let encoded = Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&')
-    console.log(encoded)
   return encoded
 }
 
@@ -73,9 +96,10 @@ export default class Contact extends React.Component {
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value })
-    if (e.target.name==='message'){
-      this.setState({ [e.target.name]: (e.target.value + ' REF is ' + imageTargetURL)  })    
-      this.setState( { 'bullshit': 'newlies'} )
+    if (e.target.name === 'message') {
+      this.setState({
+        [e.target.name]: e.target.value + ' REF is ' + imageTargetURL,
+      })
     }
   }
 
@@ -98,55 +122,66 @@ export default class Contact extends React.Component {
   render() {
     return (
       <>
-        <Container>
-          <Title>Contact form:</Title>
-          <form
-            name="contact"
-            method="post"
-            action="/thanks/"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-            onSubmit={this.handleSubmit}
-          >
-            <Inquery />
+        <Title>Contact form:</Title>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <Container>
+            <GridLeft>
+              <Inquery />
+            </GridLeft>
             {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-            <input type="hidden" name="form-name" value="contact" />
-            <p hidden>
-              <label>
-                Don’t fill this out:{' '}
-                <input name="bot-field" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your name:
-                <br />
-                <input type="text" name="contact_name" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your email:
-                <br />
-                <input type="email" name="email" onChange={this.handleChange} />
-                <input type="lies" name="bullshit" value="fuckthisshit" onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message:
-                <br />
-                <TextArea name="message" onChange={this.handleChange} />
-              </label>
-              <label>
-                <br />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        </Container>
+            <GridRight>
+              <input type="hidden" name="form-name" value="contact" />
+              <p hidden>
+                <label>
+                  Don’t fill this out:{' '}
+                  <input name="bot-field" onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your name:
+                  <br />
+                  <input
+                    type="text"
+                    name="contact_name"
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your email:
+                  <br />
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={this.handleChange}
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message:
+                  <br />
+                  <TextArea name="message" onChange={this.handleChange} />
+                </label>
+                <label>
+                  <br />
+                </label>
+              </p>
+              <p>
+                <SubmitButton type="submit">Send</SubmitButton>
+              </p>
+            </GridRight>
+          </Container>
+        </form>
       </>
     )
   }

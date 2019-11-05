50869ed272799665d161f453a82ef51edd43c656
eghan <eghant@gmail.com>
2019-11-04 16:49:37 -0800

    airtable naming improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,18 +32,18 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-netlify-cache',
     'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: 'gatsby-starter-default',
+    //     short_name: 'starter',
+    //     start_url: '/',
+    //     background_color: '#663399',
+    //     theme_color: '#663399',
+    //     display: 'minimal-ui',
+    //     //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+    //   },
+    // },
     'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-airtable`,
@@ -58,7 +58,7 @@ module.exports = {
           },
           {
             baseId: `appntJvRc0wlBRXVh`,
-            tableName: `Designs`,
+            tableName: `Data`,
             tableView: `gallery`,
             mapping: { photo: 'fileNode' },
           },
